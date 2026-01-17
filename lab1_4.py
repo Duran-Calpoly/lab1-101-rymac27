376710d6bc1cc071a0500dcdb35e23dd870ed82b
rymac27 <rymac1210@gmail.com>
2026-01-17 01:00:31 +0000

    Completed Lab 1

--- lab1_4.py
+++ lab1_4.py
@@ -1,1 +1,12 @@
-pass
+def calculate_average(num1:float, num2:float, num3:float):
+    total = num1 + num2 + num3
+    average = total / 3
+    return average
+
+def add_tax(bill_total:float):
+    total = bill_total * 1.1
+    return total
+
+def greet_user(name:str):
+    greeting = "Hello " + name
+    return greeting
--- lab1_5.py
+++ lab1_5.py
@@ -1,1 +1,23 @@
-pass
+def check_multiple(number):
+    if number % 3 == 0 and number % 5 == 0:
+        return True
+    else:
+        return False
+
+def check_password(input_string):
+    if input_string == "Python123":
+        return "access granted"
+    else:
+        return "access denied"    
+
+def calculate_federal_tax(salary):
+    if salary<=(11000):    
+        tax = salary * 0.1
+    elif salary<(44725):    
+        tax = salary * 0.12
+    elif salary<(95375):    
+        tax = salary * 0.22   
+    else:
+        tax = salary * 0.24
+    return tax
+        
